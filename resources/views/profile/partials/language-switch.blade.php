2eac0f899f10b8f65a3f287bde72677f5a3b4f30
Manuel Hertach <manuel.hertach@alea-iacta.ch>
2023-09-06 14:35:40 +0200

    Change color from indigo to hs-blue Add colors hs-blue and hs-gray to color pallet

--- resources/views/profile/partials/language-switch.blade.php
+++ resources/views/profile/partials/language-switch.blade.php
@@ -13,13 +13,13 @@
 		<?php
 		if ( request()->session()->get( 'locale' ) == 'de' ) { ?>
         <a href="{{ url('locale/de') }}"
-           class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">{{ __('language-switch.German') }}</a>
+           class="inline-flex items-center px-1 pt-1 border-b-2 border-hs-blue-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-hs-blue-700 transition duration-150 ease-in-out">{{ __('language-switch.German') }}</a>
         <a href="{{ url('locale/en') }}" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">{{ __('language-switch.English') }}</a>
         <?php
 		} else { ?>
         <a href="{{ url('locale/de') }}" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">{{ __('language-switch.German') }}</a>
         <a href="{{ url('locale/en') }}"
-           class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out">{{ __('language-switch.English') }}</a>
+           class="inline-flex items-center px-1 pt-1 border-b-2 border-hs-blue-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-hs-blue-700 transition duration-150 ease-in-out">{{ __('language-switch.English') }}</a>
         <?php } ?>
 
 
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -33,7 +33,7 @@
                     <p class="text-sm mt-2 text-gray-800">
                         {{ __('profile-information.Email_unverified') }}
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-hs-blue-500">
                             {{ __('profile-information.resend_verification_email') }}
                         </button>
                     </p>
